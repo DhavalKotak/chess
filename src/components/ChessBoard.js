d2cbe84ddd65fb4066db29bc39c4c135f3006aff
DhavalKotak <DhavalKotak@users.noreply.github.com>
2021-03-15 19:49:08 +0530

    pieces can't move outside the board

--- src/components/ChessBoard.js
+++ src/components/ChessBoard.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Tile } from './Tile/Tile'
 
 const xAxis = ["a","b","c","d","e","f","g","h"]
 const yAxis = ["1","2","3","4","5","6","7","8"]
 const pieces = []
+let currentPiece = null
 
 for(let i = 0; i < 9; i++)
     pieces.push({image: "img/b-pawn.png" , x: i, y: 6})
@@ -22,35 +23,50 @@ for(let i = 0; i < 2; i++){
     pieces.push({image: `img/${color}-rook.png`, x: 0, y: y})
 }
 
-let currentPiece = null
-
-const selectPiece = e => {
-    if(e.target.classList.contains("piece")){
-        const x = e.clientX - 50
-        const y = e.clientY - 50
-        e.target.style.position = "absolute"
-        e.target.style.top = `${y}px`
-        e.target.style.left = `${x}px`
-    }
-    currentPiece = e.target
-}
-
-const movePiece = e => {
-    if(currentPiece && currentPiece.classList.contains("piece")){
-        const x = e.clientX - 50
-        const y = e.clientY - 50
-        currentPiece.style.position = "absolute"
-        currentPiece.style.top = `${y}px`
-        currentPiece.style.left = `${x}px`
-    }
-}
-
-const releasePiece = e => {
-    if(currentPiece)
-        currentPiece = null
-}
-
 export const ChessBoard = () => {
+    const chessBoardRule = useRef(null)
+    const selectPiece = e => {
+        if(e.target.classList.contains("piece")){
+            const x = e.clientX - 50
+            const y = e.clientY - 50
+            e.target.style.position = "absolute"
+            e.target.style.top = `${y}px`
+            e.target.style.left = `${x}px`
+        }
+        currentPiece = e.target
+    }
+    
+    const movePiece = e => {
+        const chessrule = chessBoardRule.current
+        if(currentPiece && currentPiece.classList.contains("piece") && chessrule){
+            const minWidth = chessrule.offsetLeft
+            const minHeight = chessrule.offsetTop
+            const maxWidth = chessrule.clientWidth + chessrule.offsetLeft - 100
+            const maxHeight = chessrule.clientHeight + chessrule.offsetTop - 100
+            const x = e.clientX - 50
+            const y = e.clientY - 50
+            currentPiece.style.position = "absolute"
+            if(x < minWidth)
+                currentPiece.style.left = `${minWidth}px`
+            else if (x > maxWidth)
+                currentPiece.style.left = `${maxWidth}px`
+            else 
+                currentPiece.style.left = `${x}px`
+            if(y < minHeight)
+                currentPiece.style.top = `${minHeight}px`
+            else if (y > maxHeight) 
+                currentPiece.style.top = `${maxHeight}px`
+            else 
+                currentPiece.style.top = `${y}px`
+        }
+    }
+    
+    const releasePiece = e => {
+        console.log(e)
+        if(currentPiece)
+            currentPiece = null
+    }
+
     const board = []
     for(let j = yAxis.length - 1;j >= 0; j--){
         for(let i = 0;i < xAxis.length; i++){
@@ -65,7 +81,7 @@ export const ChessBoard = () => {
     }
     
     return(
-        <div className="board" onMouseDown={e => selectPiece(e)} onMouseMove={e => movePiece(e)} onMouseUp={e => releasePiece(e)}>
+        <div className="board" onMouseDown={e => selectPiece(e)} onMouseMove={e => movePiece(e)} onMouseUp={e => releasePiece(e)} ref={chessBoardRule}>
             {board}
         </div>
     )
